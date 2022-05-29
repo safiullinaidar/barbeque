be8998b805ecc8f6fda881d12819f9898406e079
Aidar Safiullin <aydarin.tatarin@gmail.com>
2022-05-29 22:49:00 +0300

    Bootstrap flash helper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,23 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error: 'alert-warning',
+      alert: 'alert-danger',
+      notice: 'alert-success'
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def bootstrap_flash
+    flash.each do |type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(type)} alert-dismissible fade show mt-4", role: 'alert') do
+        concat content_tag(:button, '',type: 'button', class: 'btn-close', data: { bs_dismiss: 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
+  
   def user_avatar(user)
     asset_path('user.png')
   end
