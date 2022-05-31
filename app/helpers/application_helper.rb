ac8dded8ebd8450c962a11d9a2ae1e9768dbae8d
Aidar Safiullin <aydarin.tatarin@gmail.com>
2022-05-31 20:55:29 +0300

    Application helper modified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,10 @@ module ApplicationHelper
       error: 'alert-warning',
       alert: 'alert-danger',
       notice: 'alert-success'
-    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+    }.stringify_keys[flash_type] || flash_type
   end
 
-  def bootstrap_flash(opts = {})
+  def bootstrap_flash
     flash.each do |type, message|
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(type)} alert-dismissible fade show mt-4", role: 'alert') do
         concat content_tag(:button, '',type: 'button', class: 'btn-close', data: { bs_dismiss: 'alert' })
