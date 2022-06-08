5525184ee604625433aa7df7e8270969c748909e
Aidar Safiullin <aydarin.tatarin@gmail.com>
2022-06-08 19:40:34 +0300

    Minor mailer localization

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,13 +4,13 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: "Новая подписка на #{event.title}"
+    mail to: event.user.email, subject: default_i18n_subject(event: @event.title)
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: "Новый комментарий #{event.title}"
+    mail to: email, subject: default_i18n_subject(event: @event.title)
   end
 end
