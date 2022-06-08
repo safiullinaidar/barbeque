07051b4cf529802d3ec29a34082f1b1e4d9d9c84
Aidar Safiullin <aydarin.tatarin@gmail.com>
2022-06-08 20:52:47 +0300

    Photo adding notification & photo url in email added

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -13,4 +13,11 @@ class EventMailer < ApplicationMailer
 
     mail to: email, subject: default_i18n_subject(event: @event.title)
   end
+
+  def photo(event, photo, email)
+    @photo = photo
+    @event = event
+
+    mail to: email, subject: default_i18n_subject(event: @event.title)
+  end
 end
