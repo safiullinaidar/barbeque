d94f4faf3e8cb656666d7ec7346121ccdc39b378
Aidar Safiullin <aydarin.tatarin@gmail.com>
2022-07-23 18:12:07 +0300

    Refactor map.js

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,20 +1,19 @@
 document.addEventListener("turbo:load", () => {
   ymaps.ready(init);
-  var myMap;
 
   function init() {
-    address = document.getElementById("map").getAttribute("data-address");
+    const address = document.getElementById("map").getAttribute("data-address");
 
-    myMap = new ymaps.Map("map", {
+    const myMap = new ymaps.Map("map", {
       center: [55.76, 37.64],
       zoom: 10
     });
 
-    myGeocoder = ymaps.geocode(address);
+    const myGeocoder = ymaps.geocode(address);
 
     myGeocoder.then(
       function (res) {
-        coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+        const coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
         myMap.geoObjects.add(
           new ymaps.Placemark(
